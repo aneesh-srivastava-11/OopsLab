a92b94d74c78adf20b87edebcebe9fb8fd2acc6b
Aneesh srivastava <aneeshplays1175@gmail.com>
2025-09-18 09:11:02 +0530

    variable remaned

--- lab7/q4.py
+++ lab7/q4.py
@@ -3,11 +3,11 @@
 def selection_sort(arr):
     n = len(arr)
     for i in range(n):
-        min_idx = i
+        minimum = i
         for j in range(i+1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+            if arr[j] < arr[minimum]:
+                minimum = j
+        arr[i], arr[minimum] = arr[minimum], arr[i]
 
 
 # ---------- Main Program ----------
